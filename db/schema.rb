91b4fd3e9e671fd8017fe40f1095dd7909a1df3a
inksprout <candy.goodison@redbubble.com>
2017-11-24 16:39:05 +1100

    user signup with secure password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170903032719) do
+ActiveRecord::Schema.define(version: 20171124044600) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "title"
@@ -24,8 +24,9 @@ ActiveRecord::Schema.define(version: 20170903032719) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
